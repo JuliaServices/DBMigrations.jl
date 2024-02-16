d183cbf9ea47969350dd46467cf37da9cd53ee90
Jacob Quinn <quinn.jacobd@gmail.com>
2024-02-15 18:41:22 -0700

    Switch to flyway compatibility

--- src/DBMigrations.jl
+++ src/DBMigrations.jl
@@ -1,45 +1,74 @@
 module DBMigrations
 
-using DBInterface, SHA
+using DBInterface, CRC32, Dates
 
 import Base: ==
 
-const MIGRATIONS_TABLE = "__migrations__"
+const MIGRATIONS_TABLE = "flyway_schema_history"
 
 const MIGRATIONS_TABLE_SCHEMA = """
 CREATE TABLE $MIGRATIONS_TABLE (
-    filename VARCHAR(255) NOT NULL,
-    sha256 VARCHAR(64) NOT NULL,
-    applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (filename)
+    installed_rank INTEGER NOT NULL,
+    version VARCHAR(50),
+    description VARCHAR(200) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    script VARCHAR(1000) NOT NULL,
+    checksum INTEGER,
+    installed_by VARCHAR(100) NOT NULL,
+    installed_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    execution_time INTEGER NOT NULL,
+    success BOOLEAN NOT NULL
 );
 """
 
-struct Sha256Mismatch <: Exception
-    filename::String
-    sha256::String
-    applied_sha256::String
+function insertmigration!(conn, m, etime)
+    DBInterface.execute(conn, "INSERT INTO $MIGRATIONS_TABLE (installed_rank, version, description, type, script, checksum, installed_by, execution_time, success) VALUES ($(m.installed_rank), '$(m.version)', '$(m.description)', '$(m.type)', '$(m.script)', $(m.checksum), '$(m.installed_by)', $(max(1, etime)), true)")
 end
 
-Base.showerror(io::IO, e::Sha256Mismatch) = print(io, "Migration file $(e.filename) has changed since it was applied to the database. Expected sha256 $(e.applied_sha256), got $(e.sha256)")
+struct ChecksumMismatch <: Exception
+    filename::String
+    checksum::Int
+    applied_checksum::Int
+end
+
+Base.showerror(io::IO, e::ChecksumMismatch) = print(io, "Migration file $(e.filename) has changed since it was applied to the database. Expected checksum $(e.applied_checksum), got $(e.checksum)")
 
 struct Migration
-    filename::String
-    sha256::String
-    applied_at::String
+    installed_rank::Int
+    version::String
+    description::String
+    type::String
+    script::String
+    checksum::Int
+    installed_by::String
+    installed_on::Union{String, DateTime}
+    execution_time::Int
+    success::Bool
     # non-db-stored fields
     statements::Vector{SubString{String}}
 end
 
+Migration(rank, version, description, type, script, checksum, installed_by, installed_on, execution_time, success) = Migration(rank, version, description, type, script, checksum, installed_by, installed_on, execution_time, success, SubString{String}[])
+
+# filename matches r"V\d+__\w+\.sql"
 function Migration(filename::String)
     contents = read(filename, String)
-    no_comments = join(filter!(x -> !startswith(x, "--"), map(strip, split(contents, '\n'))), '\n')
+    rank = parse(Int, match(r"V(\d+)", filename).captures[1])
+    version = string(rank)
+    description = match(r"V\d+__(\w+).sql", filename).captures[1]
+    lines = split(contents, '\n')
+    # calculated according to https://github.com/zaunerc/flyway-checksum-tool/blob/master/src/main/java/net/nllk/flywaychecksumtool/LoadableResource.java
+    checksum = crc32(lines[1])
+    for line in @view lines[2:end]
+        checksum = crc32(line, checksum)
+    end
+    checksum = Base.bitcast(Int32, checksum)
+    no_comments = join(filter!(x -> !startswith(x, "--"), map(strip, lines)), '\n')
     statements = filter!(!isempty, [strip(x) for x in split(no_comments, ';'; keepempty=false)])
-    return Migration(basename(filename), bytes2hex(sha256(contents)), "", statements)
+    return Migration(rank, version, description, "SQL", basename(filename), checksum, "DBMigrations.jl", "", 0, false, statements)
 end
 
-Migration(filename::String, sha256::String, applied_at) = Migration(filename, sha256, string(applied_at), SubString{String}[])
-==(m1::Migration, m2::Migration) = m1.filename == m2.filename && m1.sha256 == m2.sha256
+==(m1::Migration, m2::Migration) = m1.installed_rank == m2.installed_rank && m1.description == m2.description && m1.script == m2.script && m1.checksum == m2.checksum
 
 struct DuplicateMigrationError <: Exception
     migrations::Vector{String}
@@ -51,7 +80,7 @@ prefix(filename) = match(r"V\d+", filename).match
 
 function getmigrations(conn)
     results = DBInterface.execute(conn, "SELECT * FROM $MIGRATIONS_TABLE")
-    return [Migration(row[1], row[2], row[3]) for row in results]
+    return [Migration(row...) for row in results]
 end
 
 """
@@ -64,7 +93,7 @@ a capital `V` followed by a number, followed by two underscores, followed by a d
 migration. The number must be unique across all migrations. The description can be anything, but
 should be descriptive of the migration. The file extension currently must be `.sql`.
 
-Migration files found in `dir` will be checked against a special `__migrations__` table that
+Migration files found in `dir` will be checked against a special `$MIGRATIONS_TABLE` table that
 the DBMigrations.jl package manages in the database connection for tracking which migrations have
 already been applied. If a migration file is found in `dir` that has not been applied, it will be
 applied to the database. If a migration file is found in `dir` that has already been applied, it
@@ -98,31 +127,32 @@ function runmigrations(conn, dir::String; silent::Bool=false)
     db_index = 1
     for m in migrations
         # find matching db migration
-        while db_index <= length(dbmigrations) && prefix(dbmigrations[db_index].filename) != prefix(m.filename)
+        while db_index <= length(dbmigrations) && prefix(dbmigrations[db_index].script) != prefix(m.script)
             db_index += 1
         end
         if db_index > length(dbmigrations)
             # we checked all applied migrations and didn't find `m`, so it needs to be run
             push!(migrations_to_run, m)
-        elseif dbmigrations[db_index].sha256 != m.sha256
-            throw(Sha256Mismatch(m.filename, m.sha256, dbmigrations[db_index].sha256))
+        elseif dbmigrations[db_index].checksum != m.checksum
+            throw(ChecksumMismatch(m.script, m.checksum, dbmigrations[db_index].checksum))
         else
             # we found a matching migration, so we don't need to run it
             db_index += 1
         end
     end
     # check that resulting migrations are unique
-    allunique(prefix(m.filename) for m in migrations_to_run) || throw(DuplicateMigrationError([m.filename for m in migrations_to_run]))
+    allunique(prefix(m.script) for m in migrations_to_run) || throw(DuplicateMigrationError([m.script for m in migrations_to_run]))
     # run migrations
     for m in migrations_to_run
         DBInterface.transaction(conn) do
-            silent || @info "Applying migrations from file: $(m.filename)"
+            start = time()
+            silent || @info "Applying migrations from file: $(m.script)"
             for statement in m.statements
                 silent || @info "Applying migration statement:\n$(statement)"
                 DBInterface.execute(conn, statement)
             end
-            DBInterface.execute(conn, "INSERT INTO $MIGRATIONS_TABLE (filename, sha256) VALUES ('$(m.filename)', '$(m.sha256)')")
-            silent || @info "Applied migrations from file: $(m.filename)"
+            insertmigration!(conn, m, round(Int, time() - start))
+            silent || @info "Applied migrations from file: $(m.script)"
         end
     end
     return migrations_to_run
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,10 @@ using SQLite
         dir = "test1"
         migrations = DBMigrations.runmigrations(db, abspath(joinpath(dirname(pathof(DBMigrations)), "..", "test/sqlite", dir)))
         @test length(migrations) == 3
-        @test migrations[1].filename == "V1__baseline.sql"
-        @test migrations[2].filename == "V2__latlong.sql"
-        @test migrations[3].filename == "V3__modify.sql"
-        dbmigrations = [DBMigrations.Migration(m.filename, m.sha256, m.applied_at) for m in DBInterface.execute(db, "SELECT * FROM __migrations__")]
+        @test migrations[1].script == "V1__baseline.sql"
+        @test migrations[2].script == "V2__latlong.sql"
+        @test migrations[3].script == "V3__modify.sql"
+        dbmigrations = [DBMigrations.Migration(m...) for m in DBInterface.execute(db, "SELECT * FROM $(DBMigrations.MIGRATIONS_TABLE) ORDER BY installed_rank ASC")]
         @test migrations == dbmigrations
 
         # re-running same directory with no changes should not apply any migrations
@@ -22,7 +22,7 @@ using SQLite
         dir = "test2"
         migrations = DBMigrations.runmigrations(db, abspath(joinpath(dirname(pathof(DBMigrations)), "..", "test/sqlite", dir)))
         @test length(migrations) == 1
-        @test migrations[1].filename == "V4__modify_back.sql"
+        @test migrations[1].script == "V4__modify_back.sql"
         # test table was successfully renamed back to original name
         @test isempty(DBInterface.execute(db, "SELECT * FROM latlong"))
         @test_throws SQLiteException DBInterface.execute(db, "SELECT * FROM latlongs")
@@ -30,17 +30,17 @@ using SQLite
         dir = "test3"
         migrations = DBMigrations.runmigrations(db, abspath(joinpath(dirname(pathof(DBMigrations)), "..", "test/sqlite", dir)))
         @test length(migrations) == 1
-        @test migrations[1].filename == "V5__multiple.sql"
+        @test migrations[1].script == "V5__multiple.sql"
         for i = 1:5
             @test isempty(DBInterface.execute(db, "SELECT * FROM points$i"))
         end
 
         # Error Handling Scenarios
         # Syntax Error in Migration: Introduce a syntax error in a migration file.
-        nmigrations = length(collect(DBInterface.execute(db, "SELECT * FROM __migrations__")))
+        nmigrations = length(collect(DBInterface.execute(db, "SELECT * FROM $(DBMigrations.MIGRATIONS_TABLE)")))
         dir = "error1"
         @test_throws SQLiteException DBMigrations.runmigrations(db, abspath(joinpath(dirname(pathof(DBMigrations)), "..", "test/sqlite", dir)))
-        @test length(collect(DBInterface.execute(db, "SELECT * FROM __migrations__"))) == nmigrations
+        @test length(collect(DBInterface.execute(db, "SELECT * FROM $(DBMigrations.MIGRATIONS_TABLE)"))) == nmigrations
         @test_throws SQLiteException DBInterface.execute(db, "SELECT * FROM invalid")
 
         dir = "error2"
